42bf484e606cfbc23c2edb3863a423beea556818
bblod <walfo078@gmail.com>
2026-05-19 18:39:15 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -431,7 +431,7 @@ end)
 -- Подгрузка изолированных внешних скриптов с GitHub
 task.spawn(function()
     pcall(function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/bblodik/GhostyRustRemake/refs/heads/main/esp.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/bblodik/GhostyRustRemake/refs/heads/main/ESP.lua"))()
         -- loadstring(game:HttpGet("https://raw.githubusercontent.com/bblodik/GhostyRustRemake/main/chams.lua"))()
         -- loadstring(game:HttpGet("https://raw.githubusercontent.com/bblodik/GhostyRustRemake/main/textures.lua"))()
     end)
